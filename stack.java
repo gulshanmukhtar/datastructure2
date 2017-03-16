c6b0a202036cab0a7aed7c67cbb380bdcb433424
gulshanmukhtar <gulshanmukhtar7@gmail.com>
2017-03-16 09:22:19 -0700

    Create stack.java

---
+++ stack.java
@@ -0,0 +1,40 @@
+Public class Stack
+{
+private int size;
+private long stack array;
+private  int top;
+public Stack (int s)
+{
+size=s;
+stackArray=new long [size];
+top=-1
+}
+public void push[long];
+stack array[++top]=i;
+}public long pop()
+{
+return  stack array [top--]
+}
+public long peek()
+{
+return stackarray[top];
+}
+public boolean is empty()
+{
+return (top==-1)
+}
+public boolean is full()
+{
+return (top==size-1);
+public static void main(string{]args)
+my stack=new my stack(10)
+stack.push(10);
+stack.push(20);
+stack.push(87);
+stack.push(87);
+stack.push(55);
+while(stack is empty())
+{
+system.out.println(value)
+}
+}
